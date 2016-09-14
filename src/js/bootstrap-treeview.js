9ee7626ae8b47c2c9510149996433527fdcc102d
Patrick Riley <priley86@gmail.com>
2016-09-14 09:22:01 -0400

    version bump

--- src/js/bootstrap-treeview.js
+++ src/js/bootstrap-treeview.js
@@ -1,5 +1,5 @@
 /* =========================================================
- * bootstrap-treeview.js v2.0.0
+ * patternfly-bootstrap-treeview.js v2.1.0
  * =========================================================
  * Copyright 2013 Jonathan Miles
  * Project URL : http://www.jondmiles.com/bootstrap-treeview
